0782af88571fe0963a710a2b44113d65dcaab6c5
adaatii <80161447+adaatii@users.noreply.github.com>
2022-03-16 22:15:53 -0300

    Exemplos sobre Operadores Unários, Operadores de Atribuição, Class Math, Arredondamentos e Números Aleátórios.

---
+++ Aula#07/OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritimeticos;
+
+/**
+ *
+ * @author adaatii
+ */
+public class OperadoresAritimeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /*int n1 = 3;
+       int n2 =5;
+       float m = (n1+n2)/2;
+       
+        System.out.println("A média = " + m);*/
+        
+        /*int numero = 10;
+        int valor = 4 + numero--;        
+        System.out.println(valor);
+        System.out.println(numero);*/
+        
+        /*int x = 4;
+        int y = 5;
+        x += 2; // x = x + 2
+        y *= 2; // y = y * 2
+        System.out.println(x);
+        System.out.println(y);*/
+        
+        /*float v = 8.3f;
+        int ar = (int) Math.round(v);
+        System.out.println(ar);*/
+        
+        double ale = Math.random();
+        int n = (int) (15 + ale * (50-15));
+        System.out.println(n);
+    }
+    
+}
