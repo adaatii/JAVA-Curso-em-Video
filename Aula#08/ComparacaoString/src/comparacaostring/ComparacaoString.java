d85eca9ffb3deb1dbf30e83dca2b076ae5248033
adaatii <80161447+adaatii@users.noreply.github.com>
2022-03-21 16:31:43 -0300

    Exercicios realizados em aula Operadores Lógicos e Ternários.

---
+++ Aula#08/ComparacaoString/src/comparacaostring/ComparacaoString.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comparacaostring;
+
+/**
+ *
+ * @author adaatii
+ */
+public class ComparacaoString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome1 = "Lucas";
+        String nome2 = "Lucas";
+        String nome3 = new String ("Lucas");
+        String res;
+        res = (nome1.equals(nome3))?"igual":"diferente";
+        System.out.println(res);
+    }
+    
+}
