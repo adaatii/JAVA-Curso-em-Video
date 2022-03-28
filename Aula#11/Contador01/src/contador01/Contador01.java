e84d348f9f0d6b6a11d6b2b9b5883a138e94ff99
adaatii <80161447+adaatii@users.noreply.github.com>
2022-03-28 20:07:20 -0300

    Estrutura de repetição (while).

---
+++ Aula#11/Contador01/src/contador01/Contador01.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package contador01;
+
+/**
+ *
+ * @author adaatii
+ */
+public class Contador01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int cc = 0;
+       while(cc < 10){
+           cc++;
+           if(cc == 5 || cc == 7){
+               continue;
+           }
+           System.out.println("Cambalhota " + (cc));
+           
+       }
+    }
+    
+}
